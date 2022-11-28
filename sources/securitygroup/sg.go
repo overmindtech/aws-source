5ce9cef06e6a1ac3edc609a9385a881f6fe9ae77
David Schmitt <david.schmitt@overmind.tech>
2022-11-28 19:27:37 +0100

    Implement Get/Find/mapSecurityGroupToItem

--- sources/securitygroup/sg.go
+++ sources/securitygroup/sg.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/overmindtech/aws-source/sources"
 	"github.com/overmindtech/sdp-go"
 )
 
@@ -42,7 +44,7 @@ func (s *SecurityGroupSource) Client() *ec2.Client {
 
 // Type The type of items that this source is capable of finding
 func (s *SecurityGroupSource) Type() string {
-	return "sg-instance"
+	return "ec2-securitygroup"
 }
 
 // Descriptive name for the source, used in logging and metadata
@@ -64,20 +66,138 @@ func (s *SecurityGroupSource) Contexts() []string {
 // this source to timeout or be cancelled when executing potentially
 // long-running actions
 func (s *SecurityGroupSource) Get(ctx context.Context, itemContext string, query string) (*sdp.Item, error) {
-	return nil, &sdp.ItemRequestError{
-		ErrorType:   sdp.ItemRequestError_OTHER,
-		ErrorString: "not implemented",
-		Context:     itemContext,
+	if itemContext != s.Contexts()[0] {
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_NOCONTEXT,
+			ErrorString: fmt.Sprintf("requested context %v does not match source context %v", itemContext, s.Contexts()[0]),
+			Context:     itemContext,
+		}
 	}
+
+	describeSecurityGroupsOutput, err := s.Client().DescribeSecurityGroups(
+		ctx,
+		&ec2.DescribeSecurityGroupsInput{
+			GroupIds: []string{
+				query,
+			},
+		},
+	)
+
+	if err != nil {
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_OTHER,
+			ErrorString: err.Error(),
+			Context:     itemContext,
+		}
+	}
+
+	numSecurityGroups := len(describeSecurityGroupsOutput.SecurityGroups)
+
+	switch {
+	case numSecurityGroups > 1:
+		securityGroupIDs := make([]string, numSecurityGroups)
+
+		for i, securityGroup := range describeSecurityGroupsOutput.SecurityGroups {
+			securityGroupIDs[i] = *securityGroup.GroupId
+		}
+
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_OTHER,
+			ErrorString: fmt.Sprintf("Request returned > 1 SecurityGroup, cannot determine instance. SecurityGroups: %v", securityGroupIDs),
+			Context:     itemContext,
+		}
+	case numSecurityGroups == 0:
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_NOTFOUND,
+			ErrorString: fmt.Sprintf("SecurityGroup %v not found", query),
+			Context:     itemContext,
+		}
+	}
+
+	return mapSecurityGroupToItem(describeSecurityGroupsOutput.SecurityGroups[0], itemContext)
 }
 
 // Find Finds all items in a given context
 func (s *SecurityGroupSource) Find(ctx context.Context, itemContext string) ([]*sdp.Item, error) {
-	return nil, &sdp.ItemRequestError{
-		ErrorType:   sdp.ItemRequestError_OTHER,
-		ErrorString: "not implemented",
-		Context:     itemContext,
+	if itemContext != s.Contexts()[0] {
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_NOCONTEXT,
+			ErrorString: fmt.Sprintf("requested context %v does not match source context %v", itemContext, s.Contexts()[0]),
+			Context:     itemContext,
+		}
 	}
+
+	items := make([]*sdp.Item, 0)
+	securityGroups := make([]types.SecurityGroup, 0)
+	var maxResults int32 = 100
+	var nextToken *string
+
+	for morePages := true; morePages; {
+		describeSecurityGroupsOutput, err := s.Client().DescribeSecurityGroups(
+			ctx,
+			&ec2.DescribeSecurityGroupsInput{
+				MaxResults: &maxResults,
+				NextToken:  nextToken,
+			},
+		)
+
+		if err != nil {
+			return items, &sdp.ItemRequestError{
+				ErrorType:   sdp.ItemRequestError_OTHER,
+				ErrorString: err.Error(),
+				Context:     itemContext,
+			}
+		}
+
+		securityGroups = append(securityGroups, describeSecurityGroupsOutput.SecurityGroups...)
+
+		// If there is more data we should store the token so that we can use
+		// that. We also need to set morePages to true so that the loop runs
+		// again
+		nextToken = describeSecurityGroupsOutput.NextToken
+		morePages = (nextToken != nil)
+	}
+
+	// Convert to items
+	for _, securityGroup := range securityGroups {
+		item, _ := mapSecurityGroupToItem(securityGroup, itemContext)
+		items = append(items, item)
+	}
+
+	return items, nil
+}
+
+func mapSecurityGroupToItem(securityGroup types.SecurityGroup, itemContext string) (*sdp.Item, error) {
+	var err error
+	var attrs *sdp.ItemAttributes
+	attrs, err = sources.ToAttributesCase(securityGroup)
+
+	if err != nil {
+		return nil, &sdp.ItemRequestError{
+			ErrorType:   sdp.ItemRequestError_OTHER,
+			ErrorString: err.Error(),
+			Context:     itemContext,
+		}
+	}
+
+	item := sdp.Item{
+		Type:            "ec2-securitygroup",
+		UniqueAttribute: "groupId",
+		Context:         itemContext,
+		Attributes:      attrs,
+	}
+
+	// VPC
+	if securityGroup.VpcId != nil {
+		item.LinkedItemRequests = append(item.LinkedItemRequests, &sdp.ItemRequest{
+			Type:    "ec2-vpc",
+			Method:  sdp.RequestMethod_GET,
+			Query:   *securityGroup.VpcId,
+			Context: itemContext,
+		})
+	}
+
+	return &item, nil
 }
 
 // Weight Returns the priority weighting of items returned by this source.
