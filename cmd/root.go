6c40609b131c3a61109e0ac935121c46ad1e15a3
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2022-12-16 22:36:16 +0000

    Added key pair source

--- cmd/root.go
+++ cmd/root.go
@@ -170,6 +170,7 @@ Currently supported:
 				ec2.NewImageSource(cfg, *callerID.Account),
 				ec2.NewAddressSource(cfg, *callerID.Account),
 				ec2.NewInternetGatewaySource(cfg, *callerID.Account),
+				ec2.NewKeyPairSource(cfg, *callerID.Account),
 			}
 
 			e.AddSources(sources...)
