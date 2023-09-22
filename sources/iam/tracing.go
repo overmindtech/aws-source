17da6e8f2e410aadacec65d21b286879a583a146
David Schmitt <david.schmitt@overmind.tech>
2023-09-22 11:48:41 +0200

    Update otel; address schemaVersion merging; update to semconv 1.21.0

--- sources/iam/tracing.go
+++ sources/iam/tracing.go
@@ -2,12 +2,12 @@ package iam
 
 import (
 	"go.opentelemetry.io/otel"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
 const (
-	instrumentationName    = "github.com/overmindtech/gateway/cmd"
+	instrumentationName    = "github.com/overmindtech/aws-source/sources/iam"
 	instrumentationVersion = "0.0.1"
 )
 
