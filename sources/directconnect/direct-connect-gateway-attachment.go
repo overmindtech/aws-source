8d5932b5e42c244c5d055c7e426edf3515e59980
tomasdem <tomasdembelli@gmail.com>
2024-01-23 22:46:40 +0000

    feat(directconnect): add direct connect gateway attachment source

---
+++ sources/directconnect/direct-connect-gateway-attachment.go
@@ -0,0 +1,121 @@
+package directconnect
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/directconnect"
+	"github.com/overmindtech/aws-source/sources"
+	"github.com/overmindtech/sdp-go"
+)
+
+func directConnectGatewayAttachmentOutputMapper(_ context.Context, _ *directconnect.Client, scope string, _ *directconnect.DescribeDirectConnectGatewayAttachmentsInput, output *directconnect.DescribeDirectConnectGatewayAttachmentsOutput) ([]*sdp.Item, error) {
+	items := make([]*sdp.Item, 0)
+
+	for _, attachment := range output.DirectConnectGatewayAttachments {
+		attributes, err := sources.ToAttributesCase(attachment, "tags")
+		if err != nil {
+			return nil, err
+		}
+
+		item := sdp.Item{
+			Type:            "directconnect-direct-connect-gateway-attachment",
+			UniqueAttribute: "virtualInterfaceId", // TODO: This is not correct. Added for just passing the tests validation.
+			Attributes:      attributes,
+			Scope:           scope,
+		}
+
+		if attachment.DirectConnectGatewayId != nil {
+			// +overmind:link directconnect-direct-connect-gateway
+			item.LinkedItemQueries = append(item.LinkedItemQueries, &sdp.LinkedItemQuery{
+				Query: &sdp.Query{
+					Type:   "directconnect-direct-connect-gateway",
+					Method: sdp.QueryMethod_GET,
+					Query:  *attachment.DirectConnectGatewayId,
+					Scope:  "global",
+				},
+				BlastPropagation: &sdp.BlastPropagation{
+					// This is not clearly documented, but it seems that if the gateway is deleted, the attachment state will change to detaching
+					In: true,
+					// We can't affect the direct connect gateway
+					Out: false,
+				},
+			})
+		}
+
+		if attachment.VirtualInterfaceId != nil {
+			// +overmind:link directconnect-virtual-interface
+			item.LinkedItemQueries = append(item.LinkedItemQueries, &sdp.LinkedItemQuery{
+				Query: &sdp.Query{
+					Type:   "directconnect-virtual-interface",
+					Method: sdp.QueryMethod_GET,
+					Query:  *attachment.VirtualInterfaceId,
+					Scope:  scope,
+				},
+				BlastPropagation: &sdp.BlastPropagation{
+					// If virtual interface is deleted, the attachment state will change to detaching
+					// https://docs.aws.amazon.com/directconnect/latest/APIReference/API_DirectConnectGatewayAttachment.html#API_DirectConnectGatewayAttachment_Contents
+					In: true,
+					// We can't affect the virtual interface
+					Out: false,
+				},
+			})
+		}
+
+		items = append(items, &item)
+	}
+
+	return items, nil
+}
+
+//go:generate docgen ../../docs-data
+// +overmind:type directconnect-direct-connect-gateway-attachment
+// +overmind:descriptiveType Direct Connect Gateway Attachment
+// +overmind:get Get a direct connect gateway attachment by DirectConnectGatewayId and VirtualInterfaceId
+// +overmind:search Search direct connect gateway attachments for given VirtualInterfaceId
+// +overmind:group AWS
+
+func NewDirectConnectGatewayAttachmentSource(config aws.Config, accountID string, limit *sources.LimitBucket) *sources.DescribeOnlySource[*directconnect.DescribeDirectConnectGatewayAttachmentsInput, *directconnect.DescribeDirectConnectGatewayAttachmentsOutput, *directconnect.Client, *directconnect.Options] {
+	return &sources.DescribeOnlySource[*directconnect.DescribeDirectConnectGatewayAttachmentsInput, *directconnect.DescribeDirectConnectGatewayAttachmentsOutput, *directconnect.Client, *directconnect.Options]{
+		Config:    config,
+		Client:    directconnect.NewFromConfig(config),
+		AccountID: accountID,
+		ItemType:  "directconnect-direct-connect-gateway-attachment",
+		DescribeFunc: func(ctx context.Context, client *directconnect.Client, input *directconnect.DescribeDirectConnectGatewayAttachmentsInput) (*directconnect.DescribeDirectConnectGatewayAttachmentsOutput, error) {
+			limit.Wait(ctx) // Wait for rate limiting
+			return client.DescribeDirectConnectGatewayAttachments(ctx, input)
+		},
+		InputMapperGet: func(scope, query string) (*directconnect.DescribeDirectConnectGatewayAttachmentsInput, error) {
+			gatewayID, virtualInterfaceID, err := parseGatewayIDVirtualInterfaceID(query)
+			if err != nil {
+				return nil, fmt.Errorf(`invalid query, expected in the format of "`+gatewayIDVirtualInterfaceIDFmt, "<some_id>", "<some_id>"+`"`)
+			}
+			return &directconnect.DescribeDirectConnectGatewayAttachmentsInput{
+				DirectConnectGatewayId: &gatewayID,
+				VirtualInterfaceId:     &virtualInterfaceID,
+			}, nil
+		},
+		OutputMapper: directConnectGatewayAttachmentOutputMapper,
+		InputMapperSearch: func(ctx context.Context, client *directconnect.Client, scope, query string) (*directconnect.DescribeDirectConnectGatewayAttachmentsInput, error) {
+			virtualInterfaceID, err := parseVirtualInterfaceID(query)
+			if err != nil {
+				return nil, fmt.Errorf(`invalid query, expected in the format of "`+virtualInterfaceIDFmt+`"`, "<some_id>")
+			}
+
+			return &directconnect.DescribeDirectConnectGatewayAttachmentsInput{
+				VirtualInterfaceId: &virtualInterfaceID,
+			}, nil
+		},
+	}
+}
+
+func parseGatewayIDVirtualInterfaceID(query string) (gatewayID, virtualInterfaceID string, err error) {
+	_, err = fmt.Sscanf(query, gatewayIDVirtualInterfaceIDFmt, &gatewayID, &virtualInterfaceID)
+	return
+}
+
+func parseVirtualInterfaceID(query string) (virtualInterfaceID string, err error) {
+	_, err = fmt.Sscanf(query, virtualInterfaceIDFmt, &virtualInterfaceID)
+	return
+}
---
+++ sources/directconnect/direct-connect-gateway-attachment_test.go
@@ -0,0 +1,71 @@
+package directconnect
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/directconnect"
+	"github.com/aws/aws-sdk-go-v2/service/directconnect/types"
+	"github.com/overmindtech/aws-source/sources"
+	"github.com/overmindtech/sdp-go"
+)
+
+func TestDirectConnectGatewayAttachmentOutputMapper(t *testing.T) {
+	output := &directconnect.DescribeDirectConnectGatewayAttachmentsOutput{
+		DirectConnectGatewayAttachments: []types.DirectConnectGatewayAttachment{
+			{
+				VirtualInterfaceId:     sources.PtrString("dxvif-ffhhk74f"),
+				DirectConnectGatewayId: sources.PtrString("cf68415c-f4ae-48f2-87a7-3b52cexample"),
+				AttachmentState:        "detaching",
+			},
+		},
+	}
+
+	items, err := directConnectGatewayAttachmentOutputMapper(context.Background(), nil, "foo", nil, output)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, item := range items {
+		if err := item.Validate(); err != nil {
+			t.Error(err)
+		}
+	}
+
+	if len(items) != 1 {
+		t.Fatalf("expected 1 item, got %v", len(items))
+	}
+
+	item := items[0]
+
+	tests := sources.QueryTests{
+		{
+			ExpectedType:   "directconnect-direct-connect-gateway",
+			ExpectedMethod: sdp.QueryMethod_GET,
+			ExpectedQuery:  "cf68415c-f4ae-48f2-87a7-3b52cexample",
+			ExpectedScope:  "global",
+		},
+		{
+			ExpectedType:   "directconnect-virtual-interface",
+			ExpectedMethod: sdp.QueryMethod_GET,
+			ExpectedQuery:  "dxvif-ffhhk74f",
+			ExpectedScope:  "foo",
+		},
+	}
+
+	tests.Execute(t, item)
+}
+
+func TestNewDirectConnectGatewayAttachmentSource(t *testing.T) {
+	config, account, _ := sources.GetAutoConfig(t)
+
+	source := NewDirectConnectGatewayAttachmentSource(config, account, &TestRateLimit)
+
+	test := sources.E2ETest{
+		Source:  source,
+		Timeout: 10 * time.Second,
+	}
+
+	test.Run(t)
+}
